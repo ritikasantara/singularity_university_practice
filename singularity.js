c4fae29d14202208c4b24a41bb1a1a82826ba74f
Ritika <rits.santara@gmail.com>
2016-05-18 14:13:21 +0530

    First commit

---
+++ singularity.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function(){	
+	// $("li#about").mouseover(function(){
+		
+		
+		// $("#dropdown_about").css("visibility","visible");
+		
+		
+	// });
+	
+
+
+
+
+	$("#impact").hover(function(){
+		
+		
+		$("#dropdown_impact").css("visibility","visible");
+		
+		
+	});
+
+
+	$("#programs").hover(function(){
+		
+		
+		$("#dropdown_programs").css("visibility","visible");
+		
+		
+	});
+
+
+	$("#faculty").hover(function(){
+		
+		
+		$("#dropdown_faculty").css("visibility","visible");
+		
+		
+	});
+
+
+	$("#sulabs").hover(function(){
+		
+		
+		$("#dropdown_sulabs").css("visibility","visible");
+		
+		
+	});
+
+
+	$("#singularityhub").hover(function(){
+		
+		
+		$("#dropdown_singularity").css("visibility","visible");
+		
+		
+	});
+
+
+	$("#global").hover(function(){
+		
+		
+		$("#dropdown_global").css("visibility","visible");
+		
+		
+	});
+
+
+});
