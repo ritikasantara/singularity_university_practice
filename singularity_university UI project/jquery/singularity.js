a191331c59c11d4ae470de9d47bdc58dcb529639
ritikasantara <rits.santara@gmail.com>
2016-05-25 13:33:34 +0530

    Add files via upload

---
+++ singularity_university UI project/jquery/singularity.js
@@ -0,0 +1,121 @@
+
+$(document).ready(function(){	
+	$("#about").hover(function(){
+		
+		
+		 $(".dropdown_about").css("display","block");
+		 
+		
+	 });
+	$("#about").mouseleave(function(){
+		
+		
+		 $(".dropdown_about").css("display","none");
+		 
+		
+	 });
+
+	 
+	 
+
+
+
+	$("#impact").hover(function(){
+		
+		$(".dropdown_impact").css("display","block");
+		
+		
+	});
+
+	$("#impact").mouseleave(function(){
+		
+		
+		 $(".dropdown_impact").css("display","none");
+		 
+		
+	 });
+	 
+	$("#programs").hover(function(){
+		
+		
+		$(".dropdown_programs").css("display","block");
+		
+		
+	});
+
+	$("#programs").mouseleave(function(){
+		
+		
+		$(".dropdown_programs").css("display","none");
+		
+		
+	});
+	
+	$("#faculty").hover(function(){
+		
+		
+		$(".dropdown_faculty").css("display","block");
+		
+		
+	});
+	
+	$("#faculty").mouseleave(function(){
+		
+		
+		$(".dropdown_faculty").css("display","none");
+		
+		
+	});
+
+
+	$("#sulabs").hover(function(){
+		
+		
+		$(".dropdown_sulabs").css("display","block");
+		
+		
+	});
+    
+	$("#sulabs").mouseleave(function(){
+		
+		
+		$(".dropdown_sulabs").css("display","none");
+		
+		
+	});
+
+	$("#singuhub").hover(function(){
+		
+		
+		$(".dropdown_singularity").css("display","block");
+		
+		
+	});
+ 
+ 	$("#singuhub").mouseleave(function(){
+		
+		
+		$(".dropdown_singularity").css("display","none");
+		
+		
+	});
+
+
+	$("#global").hover(function(){
+		
+		
+		$(".dropdown_global").css("display","block");
+		
+		
+	});
+	
+	$("#global").mouseleave(function(){
+		
+		
+		$(".dropdown_global").css("display","none");
+		
+		
+	});
+
+
+});
