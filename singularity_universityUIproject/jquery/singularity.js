f2acf053d2371bb26f9bc66621003ed9ebccdd19
ritikasantara <rits.santara@gmail.com>
2016-05-30 17:00:09 +0530

    Add files via upload
    uploaded on may 30th

---
+++ singularity_universityUIproject/jquery/singularity.js
@@ -0,0 +1,455 @@
+
+$(document).ready(function(){	
+	$("#about").hover(function(){
+		
+		
+		 $(".dropdown_about").css("display","block");
+		 
+		
+	 });
+	$("#about").mouseleave(function(){
+		
+		
+		 $(".dropdown_about").css("display","none");
+		 
+		
+	 });
+
+	 
+	 
+
+
+
+	$("#impact").hover(function(){
+		
+		$(".dropdown_impact").css("display","block");
+		
+		
+	});
+
+	$("#impact").mouseleave(function(){
+		
+		
+		 $(".dropdown_impact").css("display","none");
+		 
+		
+	 });
+	 
+	$("#programs").hover(function(){
+		
+		
+		$(".dropdown_programs").css("display","block");
+		
+		
+	});
+
+	$("#programs").mouseleave(function(){
+		
+		
+		$(".dropdown_programs").css("display","none");
+		
+		
+	});
+	
+	$("#faculty").hover(function(){
+		
+		
+		$(".dropdown_faculty").css("display","block");
+		
+		
+	});
+	
+	$("#faculty").mouseleave(function(){
+		
+		
+		$(".dropdown_faculty").css("display","none");
+		
+		
+	});
+
+
+	$("#sulabs").hover(function(){
+		
+		
+		$(".dropdown_sulabs").css("display","block");
+		
+		
+	});
+    
+	$("#sulabs").mouseleave(function(){
+		
+		
+		$(".dropdown_sulabs").css("display","none");
+		
+		
+	});
+
+	$("#singuhub").hover(function(){
+		
+		
+		$(".dropdown_singularity").css("display","block");
+		
+		
+	});
+ 
+ 	$("#singuhub").mouseleave(function(){
+		
+		
+		$(".dropdown_singularity").css("display","none");
+		
+		
+	});
+
+
+	$("#global").hover(function(){
+		
+		
+		$(".dropdown_global").css("display","block");
+		
+		
+	});
+	
+	$("#global").mouseleave(function(){
+		
+		
+		$(".dropdown_global").css("display","none");
+		
+		
+	});
+	
+	//social media fb hover
+	
+	$("#social_media_fb").hover(function(){
+		
+		$("#fb_pic_container").css("background-color","#566573");
+		$(this).css("opacity","0.5");
+		
+		
+	});
+	
+	$("#social_media_fb").mouseleave(function(){
+		
+		$("#fb_pic_container").css("background-color","#566573");
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#social_media_twitter").hover(function(){
+		
+		$("#twitter_pic_container").css("background-color","#566573");
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#social_media_twitter").mouseleave(function(){
+		
+		$("#twitter_pic_container").css("background-color","#566573");
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#social_media_youtube").hover(function(){
+		
+		$("#youtube_pic_container").css("background-color","#566573");
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#social_media_youtube").mouseleave(function(){
+		
+		$("#youtube_pic_container").css("background-color","#566573");
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#social_media_gplus").hover(function(){
+		
+		$("#gplus_pic_container").css("background-color","#566573");
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#social_media_gplus").mouseleave(function(){
+		
+		$("#gplus_pic_container").css("background-color","#566573");
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#social_media_linkedin").hover(function(){
+		
+		$("#linkedin_pic_container").css("background-color","#566573");
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#social_media_linkedin").mouseleave(function(){
+		
+		$("#linkedin_pic_container").css("background-color","#566573");
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#genetech_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#genetech_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#autodesk_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#autodesk_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#google_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#google_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+
+	$("#nokia_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#nokia_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#cisco_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#cisco_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#kauffman_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#kauffman_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#eplanet_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#eplanet_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#deloitte_image").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#deloitte_image").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#TOU").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#TOU").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#PP").hover(function(){
+		
+		
+		$(this).css("opacity","0.5");
+		
+	});
+	
+	$("#PP").mouseleave(function(){
+		
+		
+		$(this).css("opacity","1");
+		
+		
+	});
+	
+	$("#button_submit_footer").hover(function(){
+		
+		
+		$(this).css("background-color","#f6b278");
+		
+	});
+	
+	$("#button_submit_footer").mouseleave(function(){
+		
+		
+		$(this).css("background-color","#f4a460");
+		
+		
+	});
+	
+	$("#button_submit").hover(function(){
+		
+		
+		$(this).css("background-color","#f6b278");
+		
+	});
+	
+	$("#button_submit").mouseleave(function(){
+		
+		
+		$(this).css("background-color","#f4a460");
+		
+		
+	});
+	
+	$("#see_all_upcomingbar").hover(function(){
+		
+		
+		$(this).css("color","#808080");
+		
+	});
+	
+	$("#see_all_upcomingbar").mouseleave(function(){
+		
+		
+		$(this).css("color","#353f47");
+		
+		
+	});
+	
+	$("#see_all_events_bar").hover(function(){
+		
+		
+		$(this).css("color","#808080");
+		
+	});
+	
+	$("#see_all_events_bar").mouseleave(function(){
+		
+		
+		$(this).css("color","#353f47");
+		
+		
+	});
+	
+	$("#daily_coverage_seeall").hover(function(){
+		
+		
+		$(this).css("color","#808080");
+		
+	});
+	
+	$("#daily_coverage_seeall").mouseleave(function(){
+		
+		
+		$(this).css("color","#353f47");
+		
+		
+	});
+	
+	$("#news_bar_seeall").hover(function(){
+		
+		
+		$(this).css("color","#808080");
+		
+	});
+	
+	$("#news_bar_seeall").mouseleave(function(){
+		
+		
+		$(this).css("color","#353f47");
+		
+		
+	});
+	
+	$("#pressbar_seeall").hover(function(){
+		
+		
+		$(this).css("color","#808080");
+		
+	});
+	
+	$("#pressbar_seeall").mouseleave(function(){
+		
+		
+		$(this).css("color","#353f47");
+		
+		
+	});
+
+
+
+});
